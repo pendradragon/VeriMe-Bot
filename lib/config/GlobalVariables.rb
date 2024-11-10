23e235e3775f69bc78e32dcd141dc733e16c95f6
Megan <megante.kerstetter@gmail.com>
2024-11-10 14:49:31 -0500

    starting the global variables and configurations commands

---
+++ lib/config/GlobalVariables.rb
@@ -0,0 +1,12 @@
+module GlobalVariables
+  @user_age = nil
+
+  def self.user_age:
+    @user_age
+  end
+
+  def self.user_age=(age):
+    @user_age = age
+  end
+
+end
--- main.rb
+++ main.rb
@@ -1,1 +1,5 @@
 require "ruby_lsp/requests/code_actions"
+
+#imports for the bots itself 
+require 'discordrb'
+require_relative 'lib/commands/GlobalVariables'
