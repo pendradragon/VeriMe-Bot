95b60122e0ff6a6876a962a2eaa29e5b5d2cd10f
Megan <megante.kerstetter@gmail.com>
2024-11-12 12:17:14 -0500

    Adding more intents

--- main.py
+++ main.py
@@ -7,7 +7,10 @@ from discord.ext import commands, tasks
 TOKEN = os.getenv("TOKEN")
 
 intents = discord.Intents.default()
-intents.message_content = True
+intents.message_content = True #For accessing message content
+intents.dm_messages = True #sending the desired user a DM
+intents.guild_messages = True #Sending messages in channels (required to keep logs)
+intents.members = True #to see when a new person joins
 
 bot = commands.Bot(command_prefix='/', intents=intents)
 
