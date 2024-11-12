6f77e1f4d3af491eddda05b4c84538436c6463cc
Megan <megante.kerstetter@gmail.com>
2024-11-12 12:14:53 -0500

    Added the basis of the bot

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ from discord.ext import commands, tasks
 
 TOKEN = os.getenv("TOKEN")
 
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='/', intents=intents)
+
 
 #starting the bot
 bot.run(TOKEN)
