edbe543cee735a2906c6abe7836518780a8ab935
Megan <megante.kerstetter@gmail.com>
2024-12-21 17:01:47 -0500

    Adding the configs directory to the main library

---
+++ lib/__init__.py
@@ -0,0 +1,3 @@
+#from the configs directory
+from .configs.base_variables_config import min_age, set_min, get_min_age
+from .configs.logs_config import log_channel_id, set_log_channel, get_log_channel
