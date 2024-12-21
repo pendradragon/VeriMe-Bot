923e7750168f2b8f34d3f66c09429cadf12f6b74
Megan <megante.kerstetter@gmail.com>
2024-12-21 15:29:09 -0500

    Creating an instance of the bot for deployment

--- main.py
+++ main.py
@@ -14,6 +14,11 @@ intents.members = True #to see when a new person joins
 
 bot = commands.Bot(command_prefix='/', intents=intents)
 
+#Creating bot instance
+@bot.event
+async def on_ready():
+        print(f"Bot is online as {bot.user}.")
+
 
 #starting the bot
 bot.run(TOKEN)
