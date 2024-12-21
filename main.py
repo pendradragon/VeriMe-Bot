b2021e31d34cc818780206a60be20f7add00a355
Megan <megante.kerstetter@gmail.com>
2024-12-21 17:07:55 -0500

    Adding set_min to the main bot commands

--- main.py
+++ main.py
@@ -4,7 +4,13 @@ import os
 import discord
 from discord.ext import commands, tasks
 
+#library imports 
+#configs directory
+from lib.configs import min_age, set_min, get_min_age
+from lib.configs import log_channel_id, set_log_channel, get_log_channel
+
 TOKEN = os.getenv("TOKEN")
+MOD_ROLE_ID = os.getenv("ROLE_ID")
 
 intents = discord.Intents.default()
 intents.message_content = True #For accessing message content
@@ -18,6 +24,23 @@ bot = commands.Bot(command_prefix='/', intents=intents)
 @bot.event
 async def on_ready():
         print(f"Bot is online as {bot.user}.")
+
+#Moderator only commands
+@bot.slash_command(name = "setage", description = "Set the global age variable -- used to compare users birthdays to.")
+async def setage(ctx, age: int):
+            """
+            This command should only be run by moderators
+            To prevent from improper use cases of the bot the role ID is unique to the 'Mods' role in my server
+
+            To prevent from abuse of duplicate IDs (if that's even a thing on Discord), the role ID is saved in a separate file
+            """
+
+            if MOD_ROLE_ID in [role.id for role in ctx.authors.roles]:
+                    response = set_min(age)
+                    await ctx.send(response)
+
+            else:
+                    await ctx.send("You lack the permissions to run this command.", ephemeral = True)
 
 
 #starting the bot
