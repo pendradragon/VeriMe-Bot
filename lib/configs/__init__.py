c7d1782a92b7de9f803b80f92b373cb148905092
Megan <megante.kerstetter@gmail.com>
2024-12-24 18:42:48 -0500

    Variable addition for date manipulation

--- lib/configs/__init__.py
+++ lib/configs/__init__.py
@@ -1,3 +1,3 @@
-from base_variables_config import min_age, set_min, get_min_age
+from base_variables_config import min_age, min_days, set_min, get_min_age
 
 from logs_config import log_channel_id, set_log_channel, get_log_channel
