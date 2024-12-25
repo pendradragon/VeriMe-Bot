ab91ac565fbd0aaf79ad5fa5f75304541524f97e
Megan <megante.kerstetter@gmail.com>
2024-12-24 19:11:44 -0500

    more complex bot setup

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ import os
 
 #discord specific imports
 import discord
+from discord import app_commands
 from discord.ext import commands, tasks
 
 #library imports 
 #configs directory
 from lib.configs import min_age, set_min, get_min_age
 from lib.configs import log_channel_id, set_log_channel, get_log_channel
+#manip directory
+from lib.manip import date_checker
 
 TOKEN = os.getenv("TOKEN")
 MOD_ROLE_ID = os.getenv("ROLE_ID")
@@ -17,6 +20,7 @@ intents = discord.Intents.default()
 intents.message_content = True #For accessing message content
 intents.dm_messages = True #sending the desired user a DM
 intents.guild_messages = True #Sending messages in channels (required to keep logs)
+intents.guild = True
 intents.members = True #to see when a new person joins
 
 bot = commands.Bot(command_prefix='/', intents=intents)
@@ -25,6 +29,12 @@ bot = commands.Bot(command_prefix='/', intents=intents)
 @bot.event
 async def on_ready():
         print(f"Bot is online as {bot.user}.")
+
+        try: 
+                synced = await bot.tree.sync()
+                print(f"Synced {len(synced)} commands.")
+        except Exception as e:
+                print(f"Failed to sync commands: {e}.")
 
 #Moderator only commands
 @bot.slash_command(name = "setage", description = "Set the global age variable -- used to compare users birthdays to.")
