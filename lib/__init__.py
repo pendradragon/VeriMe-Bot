677e0efd2eaaf7190ae1dcda25eb42c491eb85a8
Megan <megante.kerstetter@gmail.com>
2024-12-24 19:02:59 -0500

    Added the date_checker command

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,3 +1,6 @@
 #from the configs directory
 from .configs.base_variables_config import min_age, set_min, get_min_age
 from .configs.logs_config import log_channel_id, set_log_channel, get_log_channel
+
+#from the manip directory
+from .manip.date_manip import date_checker
